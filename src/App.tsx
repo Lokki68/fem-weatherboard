9b86bccab60caee124ea46972250052a4875efd9
LIBERT Brice <81553708+Lokki68@users.noreply.github.com>
2025-09-07 09:10:43 +0200

    start project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl text-red-500">Hello World</h1>
+      <button className="btn btn-soft">Click me</button>
+    </>
+  );
+}
+
+export default App;
